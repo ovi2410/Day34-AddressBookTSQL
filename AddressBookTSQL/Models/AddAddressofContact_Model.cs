fc66765dfddeefe6232716a881c50f45817934ba
ovi2410 <guneshwarifegade@gmail.com>
2022-07-29 15:56:26 +0530

    [ovi]Add:Address Book system using ER,TSQL,ADO

---
+++ AddressBookTSQL/Models/AddAddressofContact_Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookTSQL.Models
+{
+    public class AddAddressofContact_Model
+    {
+        public int PersonId { get; set; }
+        public int AddressId { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Country { get; set; }
+    }
+}
+
---
+++ AddressBookTSQL/Models/AddContact_To_Book_Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookTSQL.Models
+{
+    public class AddContact_To_Book_Model
+    {
+        public int BookId { get; set; }
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public char Gender { get; set; }
+        public long MobileNo { get; set; }
+        public int AddressId { get; set; }
+    }
+}
